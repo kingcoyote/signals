d300c0ce41ea5c15a77ac956cc1f3165f311270f
Steve Phillips <sphillips@sablesys.com>
2013-08-01 09:44:59 -0700

    moved definitions from signals.h to signals.c, since external scripts shouldn't care about this stuff. also corrected how math.h and stdlib.h are included in signals.c

--- signals.h
+++ signals.h
@@ -1,41 +1,11 @@
 #ifndef _SGFILTER_H
 #define _SGFILTER_H
 
-#include <math.h>
-#include <stdlib.h>
-
 int sgfilteri(int*, int);
 long sgfilterl(long*, int);
 float sgfilterf(float*, int);
 double sgfilterd(double*, int);
 
 #define rolling_avg(current, new, samples) (current * (samples - 1) + new) / samples
-
-#define SGF_5_1    -3
-#define SGF_5_2    12
-#define SGF_5_3    17
-#define SGF_5_4    12
-#define SGF_5_5    -3
-#define SGF_5_N    35
-
-#define SGF_7_1    -2
-#define SGF_7_2     3
-#define SGF_7_3     6
-#define SGF_7_4     7
-#define SGF_7_5     6
-#define SGF_7_6     3
-#define SGF_7_7    -2
-#define SGF_7_N    21
-
-#define SGF_9_1   -21
-#define SGF_9_2    14
-#define SGF_9_3    39
-#define SGF_9_4    54
-#define SGF_9_5    59
-#define SGF_9_6    54
-#define SGF_9_7    39
-#define SGF_9_8    14
-#define SGF_9_9   -21
-#define SGF_9_N   231
 
 #endif
