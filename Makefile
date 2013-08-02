7dc563d9bec41ccbe1d3d129599e02b337201cd9
Steve Phillips <sphillips@sablesys.com>
2013-08-02 11:21:01 -0700

    changed structure of main to support commander.c as the command line arg parser

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
-CFLAGS=-Wall -g
+CFLAGS=-Wall -g -std=c99
 CC=gcc
 
 all:
-	$(CC) $(CFLAGS) main.c signals.c -o main
+	$(CC) $(CFLAGS) main.c signals.c commander.c/src/commander.c -o main
 
 clean:
 	rm main
--- main.c
+++ main.c
@@ -4,45 +4,35 @@
 #include <time.h>
 
 #include "signals.h"
+#include "commander.c/src/commander.h"
 
-void show_help();
+void sgfilter(command_t *self);
+void outlier(command_t *self);
+void rolling(command_t *self);
 
-int main(int argc, char *argv[]) {
-    if(argc < 2) {
-        show_help();
-        return 0;
-    }
+int main(int argc, char **argv) {
+    command_t cmd;
 
-    srand(time(NULL));
+    command_init(&cmd, argv[0], "0.0.1");
+    command_option(&cmd, "-s", "--sgfilter", "Perform a Savitzky-Golay filter", sgfilter);
+    command_option(&cmd, "-o", "--outlier", "Sort the items and drop the first and last quartile", outlier);
+    command_option(&cmd, "-r", "--rolling", "Perform a rolling average on an item", rolling);
+    command_parse(&cmd, argc, argv);
 
-    if (strcmp(argv[1], "--sgfilter") == 0) {
-        int *input = malloc(sizeof(int) * 5);
-        input[0] = rand() % 200;
-        input[1] = rand() % 200 + 800;
-        input[2] = rand() % 200 + 1200;
-        input[3] = rand() % 200 + 1400;
-        input[4] = rand() % 200 + 1200;
-
-        printf("Input is: %d, %d, %d, %d, %d\n", input[0], input[1], input[2], input[3], input[4]);
-        printf("Savitzky-Golay filter:\n");
-        printf("  Window size: %i\n", 5);
-        printf("  Output: %i\n", sgfilteri(input, 5));
-        return 0;
-    } else if(strcmp(argv[1], "--outlier") == 0) {
-        printf("Outlier exclusion filter test not yet implemented\n");
-        return 0;
-    } else if (strcmp(argv[1], "--rolling") == 0) {
-        printf("Rolling average test not yet implemented\n");
-        return 0; 
-    } 
+    command_free(&cmd);
 
     return 0;
 }
 
-void show_help() {
-    printf("Usage:\n");
-    printf("  --sgfilter     Perform a test with the Savitzky-Golay filter.\n");
-    printf("  --outlier      Perform a test with outlier exclusion.\n");
-    printf("  --rolling      Perform a test with rolling averages.\n");
+void sgfilter(command_t *self) {
+
+}
+
+void outlier(command_t *self) {
+
+}
+
+void rolling(command_t *self) {
+
 }
 
