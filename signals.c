feaf5f7479399ff2209c48a8cacaf6244a73a13e
Steve Phillips <sphillips@sablesys.com>
2013-08-09 10:22:15 -0700

    began to restructure the sgfilter to not have 4 separate external functions

--- signals.c
+++ signals.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <math.h>
 
+#include "signals.h"
+
 #define SGF_5_1    -3
 #define SGF_5_2    12
 #define SGF_5_3    17
@@ -28,8 +30,31 @@
 #define SGF_9_9   -21
 #define SGF_9_N   231
 
-int sgfilteri(int* input, int window) {
-    int output = 0;
+int *sgfilter_int(int*, int);
+long sgfilter_long(long*, int);
+float sgfilter_float(float*, int);
+double sgfilter_double(double*, int);
+
+void* sgfilter(void* input, int window, datatype type) {
+    void *output;
+
+    switch(type) {
+        case DT_INT:
+            output = (void*)sgfilter_int((int*)input, window);
+            break;
+        case DT_LONG:
+            break;
+        case DT_FLOAT:
+            break;
+        case DT_DOUBLE:
+            break;
+    }
+
+    return output;
+}
+
+int *sgfilter_int(int* input, int window) {
+    int *output = 0;
 
     switch(window) {
         case 5:
@@ -64,50 +89,3 @@ int sgfilteri(int* input, int window) {
     return output;
 }
 
-long sgfilterl(long* input, int window) {
-    long output = 0;
-
-    return output;
-}
-
-float sgfilterf(float* input, int window) {
-    float output = 0.0;
-
-    return output;
-}
-
-double sgfilterd(double* input, int window) {
-    double output = 0;
-
-    switch(window) {
-        case 5:
-            output += (SGF_5_1 * input[0]) / SGF_5_N;
-            output += (SGF_5_2 * input[1]) / SGF_5_N;
-            output += (SGF_5_3 * input[2]) / SGF_5_N;
-            output += (SGF_5_4 * input[3]) / SGF_5_N;
-            output += (SGF_5_5 * input[4]) / SGF_5_N;
-            break;
-        case 7:
-            output += (SGF_7_1 * input[0]) / SGF_7_N;
-            output += (SGF_7_2 * input[1]) / SGF_7_N;
-            output += (SGF_7_3 * input[2]) / SGF_7_N;
-            output += (SGF_7_4 * input[3]) / SGF_7_N;
-            output += (SGF_7_5 * input[4]) / SGF_7_N;
-            output += (SGF_7_6 * input[5]) / SGF_7_N;
-            output += (SGF_7_7 * input[6]) / SGF_7_N;
-            break;
-        case 9:
-            output += (SGF_9_1 * input[0]) / SGF_9_N;
-            output += (SGF_9_2 * input[1]) / SGF_9_N;
-            output += (SGF_9_3 * input[2]) / SGF_9_N;
-            output += (SGF_9_4 * input[3]) / SGF_9_N;
-            output += (SGF_9_5 * input[4]) / SGF_9_N;
-            output += (SGF_9_6 * input[5]) / SGF_9_N;
-            output += (SGF_9_7 * input[6]) / SGF_9_N;
-            output += (SGF_9_8 * input[5]) / SGF_9_N;
-            output += (SGF_9_9 * input[6]) / SGF_9_N;
-            break;
-    }
-
-    return output;
-}
--- signals.h
+++ signals.h
@@ -1,10 +1,14 @@
 #ifndef _SGFILTER_H
 #define _SGFILTER_H
 
-int sgfilteri(int*, int);
-long sgfilterl(long*, int);
-float sgfilterf(float*, int);
-double sgfilterd(double*, int);
+typedef enum {
+    DT_INT,
+    DT_LONG,
+    DT_FLOAT,
+    DT_DOUBLE
+} datatype;
+
+void* sgfilter(void*, int, datatype);
 
 #define rolling_avg(current, new, samples) (current * (samples - 1) + new) / samples
 
