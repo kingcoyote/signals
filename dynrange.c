81f542d4a6878f8574eee77e31bb1c93eb70438e
Steve Phillips <sphillips@sablesys.com>
2013-08-09 11:18:08 -0700

    added dynrange function for dynamic range compression

---
+++ dynrange.c
@@ -0,0 +1,22 @@
+#include "signals.h"
+#include "dynrange.h"
+
+void dynrange(long *input, int length) {
+    int n;
+    long avg;
+    long range;
+
+    for(n = 0; n < length; n++) {
+        avg += input[n] / length;
+    }
+    
+    sort(input, length);
+    
+    range = input[length-1] - input[0];
+    
+    for(n = 0; n < length; n++) {
+        if(input[n] < avg - (range/4) || input[n] > avg + (range/4)) {
+            input[n] = avg;
+        }
+    }   
+}
---
+++ dynrange.h
@@ -0,0 +1,6 @@
+#ifndef _DYNRANGE_H_
+#define _DYNRANGE_H_
+
+void dynrange(long* input, int length);
+
+#endif
