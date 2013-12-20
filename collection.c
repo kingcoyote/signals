4a44bffafa9dfaffa6eb39c4f440e8344f4bb36b
Steve Phillips <sphillips@sablesys.com>
2013-12-20 13:06:52 -0800

    added deques and queues to the collection list

--- collection.c
+++ collection.c
@@ -8,8 +8,15 @@ stack* stack_init() {
     return s;
 }
 
+void stack_destroy(stack* s) {
+    while (s->size > 0) {
+        stack_pop(s);
+    }
+    free(s);
+}
+
 void stack_push(stack* s, void* n) {
-    struct _stack_node* node = malloc(sizeof(struct _stack_node*));
+    struct _stackNode* node = malloc(sizeof(struct _stackNode*));
     node->item = n;
     node->next = s->first;
     s->first = node;
@@ -17,7 +24,7 @@ void stack_push(stack* s, void* n) {
 }
 
 void* stack_pop(stack* s) {
-    struct _stack_node* n = s->first;
+    struct _stackNode* n = s->first;
     s->first = n->next;
     s->size--;
     void* item = n->item;
@@ -29,10 +36,130 @@ int stack_size(stack* s) {
     return s->size;
 }
 
-void stack_destroy(stack* s) {
-    free(s);
+int stack_isEmpty(stack* s) {
+    return s->size == 0;
+}
 
-    while (s->size > 0) {
-        stack_pop(s);
+queue* queue_init() {
+    queue* q= malloc(sizeof(queue*));
+    q->size = 0;
+
+    return q;
+}
+
+void queue_destroy(queue* q) {
+    while(!queue_isEmpty(q)) {
+        queue_dequeue(q);
     }
+
+    free(q);
+}
+
+void queue_enqueue(queue* q, void *n) {
+    struct _queueNode* node = malloc(sizeof(struct _queueNode*));
+    node->item = n;
+    node->next = q->last;
+    q->last = node;
+    q->size++;
+}
+
+void* queue_dequeue(queue* q) {
+    struct _queueNode* node = q->first;
+    q->first = node->next;
+    void* item = node->item;
+    free(node);
+    q->size--;
+    return item;
+}
+
+int queue_size(queue* q) {
+    return q->size;
+}
+
+int queue_isEmpty(queue* q) {
+    return q->size == 0;
+}
+
+deque* deque_init() {
+    deque* d = malloc(sizeof(deque*));
+    d->size = 0;
+    return d;
+}
+
+void deque_destroy(deque* d) {
+    while (!deque_isEmpty(d)) {
+        deque_popFront(d);
+    }
+
+    free(d);
+}
+
+void deque_pushFront(deque* d, void* n) {
+    struct _dequeNode* node = malloc(sizeof(struct _dequeNode*));
+    node->item = n;
+    node->next = d->first;
+    d->first->prev = node;
+    d->first = node;
+    d->size++;
+
+    if (d->size == 1) {
+        d->last = d->first;
+    }
+}
+
+void deque_pushBack(deque* d, void* n) {
+    struct _dequeNode* node = malloc(sizeof(struct _dequeNode*));
+    node->item = n;
+    node->prev = d->last;
+    d->last->next = node;
+    d->last = node;
+    d->size++;
+
+    if (d->size == 1) {
+        d->first = d->last;
+    }
+}
+
+void* deque_popFront(deque* d) {
+    struct _dequeNode* node = d->first;
+    void* item = node->item;
+
+    d->size--;
+    if (d->size > 1) {
+        d->first = node->next;
+    } else {
+        d->first = d->last;
+    }
+
+    d->first->prev = NULL;
+
+    free(node);
+
+    return item;
+}
+
+void* deque_popBack(deque* d) {
+    struct _dequeNode* node = d->last;
+    void* item = node->item;
+
+    d->size--;
+    if (d->size > 1) {
+        d->last = node->prev;
+    } else {
+        d->last = d->first;
+    }
+
+    d->last->next = NULL;
+
+    free(node);
+
+    return item;
+}
+
+int deque_size(deque* d) {
+    return d->size;
+}
+
+int deque_isEmpty(deque* d) {
+    return deque_size(d) == 0;
 }
--- collection.h
+++ collection.h
@@ -1,22 +1,72 @@
 #ifndef __COLLECTION_H_
 #define __COLLECTION_H_
 
-struct _stack_node {
-    struct _stack_node* next;
+// Begin Stack headers
+
+struct _stackNode {
+    struct _stackNode* next;
     void* item;
 };
 
 struct _stack {
     int size;
-    struct _stack_node* first;
+    struct _stackNode* first;
 };
 
 typedef struct _stack stack;
 
 stack*  stack_init();
+void    stack_destroy(stack* s);
 void    stack_push(stack* s, void* n);
 void*   stack_pop(stack* s);
 int     stack_size(stack* s);
-void    stack_destroy(stack* s);
+int     stack_isEmpty(stack* s);
+
+// Begin Queue headers
+
+struct _queueNode {
+    struct _queueNode* next;
+    void* item;
+};
+
+struct _queue {
+    int size;
+    struct _queueNode* first;
+    struct _queueNode* last;
+};
+
+typedef struct _queue queue;
+
+queue*  queue_init();
+void    queue_destroy(queue* q);
+void    queue_enqueue(queue* q, void *n);
+void*   queue_dequeue(queue* q);
+int     queue_size(queue* q);
+int     queue_isEmpty(queue* q);
+
+// Begin Deque headers
+
+struct _dequeNode {
+    struct _dequeNode* prev;
+    struct _dequeNode* next;
+    void* item;
+};
+
+struct _deque {
+    int size;
+    struct _dequeNode* first;
+    struct _dequeNode* last;
+};
+
+typedef struct _deque deque;
+
+deque*  deque_init();
+void    deque_destroy(deque* d);
+void    deque_pushFront(deque* d, void* n);
+void    deque_pushBack(deque* d, void* n);
+void*   deque_popFront(deque* d);
+void*   deque_popBack(deque* d);
+int     deque_size(deque* d);
+int     deque_isEmpty(deque* d);
 
 #endif
